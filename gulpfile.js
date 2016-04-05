642d59dddbd754e9e0c134fbb017e6c1cf76120a
lijian <aimilixi@foxmail.com>
2016-04-05 23:32:49 +0800

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var elixir = require('laravel-elixir');
+
+/*
+ |--------------------------------------------------------------------------
+ | Elixir Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Elixir provides a clean, fluent API for defining some basic Gulp tasks
+ | for your Laravel application. By default, we are compiling the Less
+ | file for our application, as well as publishing vendor resources.
+ |
+ */
+
+elixir.config.sourcemaps = false;
+
+elixir(function(mix) {
+    mix.sass(['app.scss','button.scss'])
+       .version('css/app.css');
+});
